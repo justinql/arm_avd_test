6255f02e4058ed06d6236e6bc6e9dddc0adfac3a
carlosantana07 <40328838+carlosantana07@users.noreply.github.com>
2023-03-09 13:00:40 -0500

    install
    chocolatey
    create update task for office & chocolatey

--- AVD.ps1
+++ AVD.ps1
@@ -1,6 +1,29 @@
+
+#forcing the execution
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
 #installing chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-#
+
+#getting current directory
+$CurrentLocation= Get-Location 
+
+#Chocolately xmlfile downlaod
+$URL= "https://raw.githubusercontent.com/justinql/arm_avd_test/scheduled-taks/Chocolately_upgrade.xml" 
+Invoke-WebRequest $URL -OutFile "Chocolately_upgrade.xml" 
+
+
+#office xmlfile update
+$URL2= "https://raw.githubusercontent.com/justinql/arm_avd_test/scheduled-taks/Microsoft_Office_weekly_update.xml" 
+Invoke-WebRequest $URL2 -OutFile "Microsoft_Office_weekly_update.xml" 
+
+
+
+#creating chocolately task
+Register-ScheduledTask -Xml (get-content $CurrentLocation"\Chocolately_upgrade.xml" | out-string) -TaskName "Chocolately upgrade" -TaskPath "\Chocolatety" -force
+
+#creating office task
+Register-ScheduledTask -Xml (get-content $CurrentLocation"\Microsoft_Office_weekly_update.xml" | out-string) -TaskName "Microsoft Office weekly update"  -TaskPath "\OfficeUpdate" -force
+
+
